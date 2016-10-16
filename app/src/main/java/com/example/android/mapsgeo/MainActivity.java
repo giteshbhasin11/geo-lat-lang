c3b2b0491bbbd507730ff99c3dc9dfad68d5d68f
gitesh bhasin <gitesh bhasin>
2016-10-17 03:04:29 +0530

    Half app made

---
+++ app/src/main/java/com/example/android/mapsgeo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.android.mapsgeo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textLat;
+    TextView textLong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textLat = (TextView) findViewById(R.id.textViewLat);
+        textLong = (TextView) findViewById(R.id.textViewLong);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener ll = new myLocationListener();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+    }
+
+        class myLocationListener implements LocationListener {
+            @Override
+            public void onLocationChanged(Location location) {
+                if(location != null) {
+                    double pLong = location.getLongitude();
+                    double pLat = location.getLongitude();
+
+                    textLat.setText(Double.toString(pLat));
+                    textLong.setText(Double.toString(pLong));
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        }
+
+
+    }
+
